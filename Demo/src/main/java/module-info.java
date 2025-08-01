0c5a7977e1bf519ba041980ab50e2aee64346f06
Md Sadique Hossain <sadique21.hossain@gmail.com>
2025-08-01 10:42:28 +0600

    Updated

--- Demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sdk {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sdk to javafx.fxml;
-    exports com.example.sdk;
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Rep_demo
-
-This is my first Repository.
-Author - Md Sadique Hossain
