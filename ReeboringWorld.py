c53c9b5e8fe8ffcd24844b60396c03558146133e
Md Sadique Hossain <sadique21.hossain@gmail.com>
2025-08-03 10:22:40 +0600

    Updated

---
+++ ReeboringWorld.py
@@ -0,0 +1,23 @@
+
+def turn_aro():
+    turn_left()
+    turn_left()    
+def func():
+    turn_aro()
+    turn_left()
+    move()
+    turn_left()
+    turn_aro()
+    move()
+def rep():
+    move()
+    turn_left()
+    move()
+    func()
+    turn_left()        
+rep()   
+rep()
+rep()
+rep()
+rep()
+rep()
